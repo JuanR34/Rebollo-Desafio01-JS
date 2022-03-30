fd26ef252571164ec1fe8da2295a4e17a75784ae
JuanR34 <juanpablo_jpr@hotmail.com>
2022-03-30 01:32:37 -0300

    Desfio 1 - Rebollo

---
+++ js/miarchivo.js
@@ -0,0 +1,51 @@
+function saludar  ( nombre = prompt ("Hola. Cual es tu nombre?")) {
+    let frase = (`Bienvenido ${nombre} a DivExChange. Tenemos las mejores cotizaciones del mercado! `);
+    alert (frase);
+    document.write (frase);  
+}
+
+saludar();
+
+let cotizacion = prompt ("Elija tipo de cotizacion: oficial o blue");
+
+if (cotizacion === "oficial") {
+    alert ("Recuerde: para la compra de dolares al valor oficial, se le recargan los impuestos país y el solidario")
+}
+let dolarOficial = 106;
+let dolarBlue = 201;
+let operacion = prompt ("Usted quiere comprar o vender dólares?");
+
+if (operacion === "vender") {
+    alert ("La venta de dolares se toman al tipo de cambio del dolar blue")
+}
+
+let monto = parseInt (prompt ("ingrese su monto en dólares"));
+
+
+// COMPRA DE DOLARES 
+
+while (monto > 0 && operacion === ("comprar")) { 
+   if  (cotizacion === "oficial") {
+   resultado =  dolarOficial * monto;
+   resultadoA = resultado + (resultado * 0.63);
+    alert ("el monto en pesos a pagar es  $" + resultadoA);
+    break
+   } else if (cotizacion === "blue") {
+       resultadoB = dolarBlue * monto;
+       alert("el monto en pesos a pagar es  $" + resultadoB);
+       break
+   }
+}
+
+
+//VENTA DE DOLARES
+
+while (monto > 0 && operacion === ("vender")) { 
+    if (cotizacion === "blue" || cotizacion === "oficial") {
+        resultadoB = monto * dolarBlue;
+        alert("El monto a recibir en pesos es $ " + resultadoB);
+        break
+    } 
+ }
+
+
